ece571efeeccfeafece053d4c4795e9a40cc4bd9
Heechul So <thrrkthrrk@gmail.com>
2020-08-06 13:42:19 +0900

    Initialize

---
+++ src/test/java/bet/bestdata/game/destiny/DestinyApplicationTests.java
@@ -0,0 +1,13 @@
+package bet.bestdata.game.destiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
